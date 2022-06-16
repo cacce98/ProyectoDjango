e1e630f639e309978ec5950f36f8e0cf1188bda8
cacce98 <carol98andres@gmail.com>
2022-06-15 23:20:41 -0500

    segundo commit

--- prueba/inicio/views.py
+++ prueba/inicio/views.py
@@ -1,19 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
 
-menu="""
-<a href="/">Home</a>
-<a href="/contacto">Contactanos</a>"""
+
 
 # Create your views here.
 
 def principal(request):
-    contenido="<h1>Hola Django</h1>"
-    return HttpResponse(menu+contenido)
+    
+    return render(request, "inicio/principal.html")
 
 def contacto(request):
-    contenido="""<h2>Contacto</h2>
-    <p>Nombre:<input type="text" name="nombre"></p>
-    <p>Mensaje:</p><p><textarea cols="50" rows="10"></textarea></p>
-    <p><input type="button" name="enviar" value="Enviar"/></p>"""
-    return HttpResponse(menu+contenido)
+
+    return render(request, "inicio/contacto.html")
